62fbaf25cbf6dcff6bff82829b9d67e7ed82c859
Kaj <kst87@users.noreply.github.com>
2018-03-24 14:23:46 +0100

    Update updatestofrei.sh

--- updatestofrei.sh
+++ updatestofrei.sh
@@ -22,43 +22,9 @@ then
     fi
 fi
 
-
-if ! cmp $GIT_REPO/db.net.freifunk.badoldesloe /etc/bind/db.net.freifunk.badoldesloe >/dev/null 2>&1 ;
-then
-    cp $GIT_REPO/db.net.freifunk.badoldesloe /etc/bind/db.net.freifunk.badoldesloe
-
-    /usr/sbin/service bind9 status 2>&1> /dev/null
-    if [[ $? -eq 0 ]]
-    then
-        /usr/sbin/service bind9 restart
-    fi
-fi
-
 if ! cmp $GIT_REPO/db.de.freifunk-stormarn /etc/bind/db.de.freifunk-stormarn >/dev/null 2>&1 ;
 then
     cp $GIT_REPO/db.de.freifunk-stormarn /etc/bind/db.de.freifunk-stormarn
-
-    /usr/sbin/service bind9 status 2>&1> /dev/null
-    if [[ $? -eq 0 ]]
-    then
-        /usr/sbin/service bind9 restart
-    fi
-fi
-
-if ! cmp $GIT_REPO/db.de.freifunk-badoldesloe /etc/bind/db.de.freifunk-badoldesloe >/dev/null 2>&1 ;
-then
-    cp $GIT_REPO/db.de.freifunk-badoldesloe /etc/bind/db.de.freifunk-badoldesloe
-
-    /usr/sbin/service bind9 status 2>&1> /dev/null
-    if [[ $? -eq 0 ]]
-    then
-        /usr/sbin/service bind9 restart
-    fi
-fi
-
-if ! cmp $GIT_REPO/db.net.freifunk.reinbek /etc/bind/db.net.freifunk.reinbek >/dev/null 2>&1 ;
-then
-    cp $GIT_REPO/db.net.freifunk.reinbek /etc/bind/db.net.freifunk.reinbek
 
     /usr/sbin/service bind9 status 2>&1> /dev/null
     if [[ $? -eq 0 ]]
